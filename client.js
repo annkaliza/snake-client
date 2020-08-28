1804e6cd08cec9bc92c2ac876e4777c634270e15
Kaliza Anna <annkaliza@gmail.com>
2020-08-28 10:14:10 +0000

    refactored play.js and add client.js file

--- client.js
+++ client.js
@@ -1,4 +1,15 @@
-// play.js
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = { connect };
