6921edecf63b17f172f842488d9f20ec519c6f9f
Kaliza Anna <annkaliza@gmail.com>
2020-08-28 12:21:48 +0000

    add ctrl + c handler

--- play.js
+++ play.js
@@ -1,12 +1,30 @@
 // play.js
-const { connect } = require('./client');
-console.log('Connecting ...');
+const { connect } = require("./client");
+console.log("Connecting ...");
 connect();
 
-connect().on("Move: up", function(){
+// connect().on("Move: up", function(){
 
-});
+// });
 
 // connect().on("Name:AKA", function(){
 
 // });
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => handleUserInput(key));
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+};
+
+setupInput();
