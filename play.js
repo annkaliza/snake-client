50d1ffb44390c0a85857d4e599585f4d42ec8e84
Kaliza Anna <annkaliza@gmail.com>
2020-08-28 12:56:50 +0000

    separated server and input functionalities in different files

--- play.js
+++ play.js
@@ -1,30 +1,9 @@
-// play.js
+const { setupInput } = require('./input');
 const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
 
-// connect().on("Move: up", function(){
 
-// });
 
-// connect().on("Name:AKA", function(){
-
-// });
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (key) => handleUserInput(key));
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-};
 
 setupInput();
