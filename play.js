61924a438fa3e493df9b5dc5e27ebd0bfdff0b5f
Kaliza Anna <annkaliza@gmail.com>
2020-08-28 10:54:21 +0000

    add move up functionality

--- play.js
+++ play.js
@@ -3,6 +3,10 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
-connect().on("Name:AKA", function(){
+connect().on("Move: up", function(){
 
 });
+
+// connect().on("Name:AKA", function(){
+
+// });
